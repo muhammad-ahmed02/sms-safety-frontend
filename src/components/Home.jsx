57af4b93426f186c5c49ead5637602da64ee0cc6
SheikhAhmed135 <shiekhahmed135@gmail.com>
2021-12-10 18:15:04 +0500

    comming soon

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import "./Home.scss"
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Comming Soon...</h1>
+      <p>
+        Our website is currently under construction. We will be going live soon.
+        Thank you for your patience.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
