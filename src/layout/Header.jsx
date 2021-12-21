279ce91b7231c2a136825bf2c9506ed432a20218
SheikhAhmed135 <shiekhahmed135@gmail.com>
2021-12-21 13:41:32 +0500

    Production push

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,21 +1,36 @@
 import { Link } from "react-router-dom";
+
+import useSite from "../hooks/site";
 import useHome from "../hooks/home";
 
 const Header = () => {
   const home = useHome();
+  const site = useSite();
 
   return (
-    <header>
-      {home.hasLoaded && (
-        <div className="logo">
-          {home.state.map((logo, i) => (
-            <Link to="/" key={i}>
-              <img src={logo.logo} alt="Logo" width="120px" />
-            </Link>
+    <>
+      {site.hasLoaded && (
+        <header>
+          {site.state.map((st, i) => (
+            <div key={i}>
+              {st.status !== "Down" && (
+                <>
+                  {home.hasLoaded && (
+                    <div className="logo">
+                      {home.state.map((logo, i) => (
+                        <Link to="/" key={i}>
+                          <img src={logo.logo} alt="Logo" width="120px" />
+                        </Link>
+                      ))}
+                    </div>
+                  )}
+                </>
+              )}
+            </div>
           ))}
-        </div>
+        </header>
       )}
-    </header>
+    </>
   );
 };
 
